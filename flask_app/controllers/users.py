1fd098bc41aae6fa572c83850839b8fbfa835d63
ncapel <capel.nicholas@gmail.com>
2024-01-06 15:23:08 -0500

    removed option for plus_1, updated stripe donation to official one.

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -30,36 +30,10 @@ def register():
     data ={
         "name": request.form['name'],
         "email": request.form['email'],
-        "plus_1": request.form['plus_1'],
         "food": request.form['food'],
-        "plus_one_food": request.form['plus_one_food'],
         "invite_code": request.form['invite_code']
     }
     id = User.save(data)
     session['user_id'] = id
 
     return redirect('/')
-
-# @app.route('/admin')
-# def admin_login():
-#     return render_template('login.html')
-
-# @app.route('/confirm-admin',methods=['POST'])
-# def admin():
-
-#     if not User.validate_admin(request.form):
-#         return redirect('/admin')
-#     data ={
-#         "user": request.form['user'],
-#         "pass": request.form['pass']
-#     }
-#     id = User.save(data)
-#     session['user_id'] = id
-
-#     return redirect('/rsvpinfo')
-
-# @app.route('/rsvpinfo')
-# def adminpanel():
-#     all_users = User.get_all()
-#     plus_ones = User.get_all_plus_ones()
-#     return render_template('adminpanel.html',all_users=all_users,plus_ones=plus_ones)
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -10,30 +10,18 @@ class User:
         self.id = data['id']
         self.name = data['name']
         self.email = data['email']
-        self.plus_one = data['plus_1']
         self.food_option = data['food']
-        self.plus_one_food = data['plus_one_food']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO users (name,email,plus_1,food,plus_one_food,created_at) VALUES(%(name)s,%(email)s,%(plus_1)s,%(food)s,%(plus_one_food)s,NOW())"
+        query = "INSERT INTO users (name,email,food,created_at,updated_at) VALUES(%(name)s,%(email)s,%(food)s,NOW(),NOW());"
         return connectToMySQL(cls.db).query_db(query,data)
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL(cls.db).query_db(query)
-        users = []
-        for row in results:
-            users.append( cls(row))
-        print(users)
-        return users
-
-    @classmethod
-    def get_all_plus_ones(cls):
-        query = "SELECT * FROM users WHERE plus_1 != 0;"
         results = connectToMySQL(cls.db).query_db(query)
         users = []
         for row in results:
@@ -71,12 +59,6 @@ class User:
             is_valid= False
         if len(user['food']) < 1:
             flash("Select a food option","rsvp")
-            is_valid= False
-        if not user['plus_1'] != '1' and user['plus_1'] != '0':
-            flash("Select a valid guest option","rsvp")
-            is_valid= False
-        if user['plus_1'] == 1 and len(user['plus_one_food']) < 1:
-            flash("Select a food option for your guest","rsvp")
             is_valid= False
         if user['invite_code'] != 'fields24':
             flash("Please enter your invite code","rsvp")
